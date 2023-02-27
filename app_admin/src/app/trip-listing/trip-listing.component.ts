bf39ab2394f2f93d5d7b2540d16169f0cb07c67e
MattMuller88 <mattmuller1188@gmail.com>
2023-02-26 21:40:17 -0500

    module 7 complete

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
-//import { AuthenticationService } from '../services/authentication';
+import { AuthenticationService } from '../services/authentication';
 
 @Component({
   selector: 'app-trip-listing',
@@ -18,7 +18,7 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
-    //private authService: AuthenticationService,
+    private authService: AuthenticationService,
     private router: Router
     ) { }
 
@@ -38,53 +38,12 @@ export class TripListingComponent implements OnInit {
       });
   }
 
-  //public isLoggedIn(): boolean {
-  //  return this.authService.isLoggedIn();
-  //}
+  public isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 
   ngOnInit() {
     this.getTrips();
   }
 
 }
-
-// import { Component, OnInit } from "@angular/core";
-// import { Router } from "@angular/router";
-// //import { trips } from "../data/trips";
-// import { TripDataService } from "../services/trip-data.service";
-// import { Trip } from "../models/trip";
-
-// @Component({
-//   selector: "app-trip-listing",
-//   templateUrl: "./trip-listing.component.html",
-//   styleUrls: ["./trip-listing.component.css"],
-//   providers: [TripDataService],
-// })
-// export class TripListingComponent implements OnInit {
-//   //trips: Array<any> = trips;
-//   trips: Trip[];
-
-//   message: string;
-
-//   constructor(
-//     private tripDataService: TripDataService,
-//     private router: Router
-//   ) {}
-
-//   private addTrip(): void {
-//     this.router.navigate(["add-trip"]);
-//   }
-
-//   private getTrips(): void {
-//     console.log("Inside TripListingComponent#getTrips");
-//     this.message = "Searching for trips";
-//     this.tripDataService.getTrips().then((foundTrips) => {
-//       this.message = foundTrips.length > 0 ? "" : "No trips found";
-//       this.trips = foundTrips;
-//     });
-//   }
-
-//   ngOnInit(): void {
-//     this.getTrips();
-//   }
-// }
